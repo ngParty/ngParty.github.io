d9e1d21b72a3821805934c5aa49fafb91c0ef98d
Martin Hochel <hochelmartin@gmail.com>
2020-02-13 22:40:19 +0100

    ci: publish new version

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9e58e442bd21950804a6";
+/******/ 	var hotCurrentHash = "4e085df8a5fbb149198c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
